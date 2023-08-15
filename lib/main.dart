e13cf3ca80bbb5aad7df7171b742dc2c7f4a6325
akashKarmakar02 <akash.anan03@gmail.com>
2023-08-15 20:50:23 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/controller/auth_controller.dart';
 import 'package:tiktok_clone/views/screens/auth/signup_screen.dart';
 import "package:tiktok_clone/firebase_options.dart";
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  ).then((value) => Get.put(AuthController()));
   runApp(const MyApp());
 }
 
